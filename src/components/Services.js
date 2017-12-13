8a1d3d8f809edee5becb6c73c8fa55e66906ba25
Alexis <ali.finney@gmail.com>
2017-12-13 12:40:15 -0700

    afternoon sync

--- src/components/Services.js
+++ src/components/Services.js
@@ -15,25 +15,23 @@ class Services extends Component {
         <Parallax bgImage={image1}
         strength={500}>
         <div style={{height: 500}}>
-          <div className="ServicesContainer">
-          <div className="PersonalTrain"> 
-           <h2>Personal Training</h2>
-             <p>This package is for Individuals that are truly commited to change.  I require a deep level of commitment from all of my clients to make sure they are fully enrolled and commited to changing their lifestyle.  It is only here that we will get true lasting results.</p>
-             <button className="AboutButton">Train With Billy</button>
-           </div>
-           <div className="OnlineTrain">
-             <h2>Online Training Design</h2>
-               <p> A good majority of people have gym "box" memberships, and get no use out of them.  What if you had a cheap way to get a 4-week training program custom to your goals emailed to you every month?  Well now you do!  Let Billy maximize your results at the gym of your choice, and on your time without the heavy cost of personal training.    </p>
-               <button className="AboutButton">Learn More</button>
-           </div>
-            </div>
+        <div className="choosePlan">
+          <h1>Choose A Plan That's Right For You</h1>
+        </div>
         </div>
       </Parallax>
-        <div>
-        
-        </div>
-        <Title />
-        <TopNav />
+          <div className="ServicesContainer">
+            <div className="PersonalTrain"> 
+            <h2>Personal Training</h2>
+              <p>This package is for Individuals that are truly commited to change.  I require a deep level of commitment from all of my clients to make sure they are fully enrolled and commited to changing their lifestyle.  It is only here that we will get true lasting results.</p>
+             
+            </div>
+            <div className="OnlineTrain">
+              <h2>Online Training Design</h2>
+                <p> A good majority of people have gym "box" memberships, and get no use out of them.  What if you had a cheap way to get a 4-week training program custom to your goals emailed to you every month?  Well now you do!  Let Billy maximize your results at the gym of your choice, and on your time without the heavy cost of personal training.    </p>
+                
+            </div>
+          </div>
         <Footer />
       </div>
     )
